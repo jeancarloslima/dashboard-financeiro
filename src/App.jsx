ed6e43c4002c786373213dc08ecc2f100e2946fd
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-08 12:52:50 -0300

    Definindo estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import './App.css'
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <header>
+        <div className="header-container">
+          <ResumoFinanceiro />
+        </div>
+      </header>
+
+      <main>
+        <div className="main-container">
+          <FormularioTransacao />
+          <ListaTransacoes />
+        </div>
+      </main>
+    </div>
   )
 }
 
