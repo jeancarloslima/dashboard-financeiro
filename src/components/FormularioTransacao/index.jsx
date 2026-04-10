0f0b15f39405d6ca57ed45d3d00494dec81252fd
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-10 13:48:03 -0300

    Adicionando estilo ao componente Formulario Transacao

--- src/components/FormularioTransacao/index.jsx
+++ src/components/FormularioTransacao/index.jsx
@@ -2,32 +2,84 @@ import { useState } from "react";
 import styles from "./FormularioTransacao.module.css";
 
 export default function FormularioTransacao() {
-    const [formularioAberto, setFormularioAberto] = useState(false);
+  const [formularioAberto, setFormularioAberto] = useState(false);
+  const [tipo, setTipo] = useState("Entrada");
 
-    return (
-        <div className={styles.container}>
-            {!formularioAberto && <button onClick={() => setFormularioAberto(true)} className={styles.botaoAbreFormulario}>Adicionar Transação</button>}
-            {formularioAberto && (
-                <form className={styles.formulario}>
-                    <div className={styles.containerInputFormulario}>
-                        <label htmlFor="descricao">Descrição</label>
-                        <input type="text" name="descricao" id="descricao" autoFocus required />
-                    </div>
-                    <div className={styles.containerInputFormulario}>
-                        <label htmlFor="valor">Valor</label>
-                        <input type="number" name="valor" id="valor" required />
-                    </div>
-                    <div className={styles.containerInputFormulario}>
-                        <label>Tipo</label>
-                        <button>Entrada</button>
-                        <button>Saída</button>
-                    </div>
-                    <div className={styles.containerBotoesFormulario}>
-                        <button>Cancelar</button>
-                        <button type="submit">Adicionar</button>
-                    </div>
-                </form>
-            )}
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      {!formularioAberto && (
+        <button
+          onClick={() => setFormularioAberto(true)}
+          className={styles.botaoAbreFormulario}
+        >
+          Adicionar Transação
+        </button>
+      )}
+      {formularioAberto && (
+        <form className={styles.formulario}>
+          <div className={styles.containerInputFormulario}>
+            <label className={styles.label} htmlFor="descricao">
+              Descrição
+            </label>
+            <input
+              className={styles.inputDescricao}
+              type="text"
+              name="descricao"
+              id="descricao"
+              autoFocus
+              required
+            />
+          </div>
+          <div className={styles.containerInputFormulario}>
+            <label htmlFor="valor">Valor</label>
+            <input
+              className={styles.inputValor}
+              type="number"
+              name="valor"
+              id="valor"
+              required
+            />
+          </div>
+          <div className={styles.containerInputFormulario}>
+            <label className={styles.label}>Tipo</label>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setTipo("Entrada");
+              }}
+              className={`${styles.btnTipo} ${styles.btnEntrada} ${tipo === "Entrada" ? styles.ativo : ""}`}
+            >
+              Entrada
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setTipo("Saida");
+              }}
+              className={`${styles.btnTipo} ${styles.btnSaida} ${tipo === "Saida" ? styles.ativo : ""}`}
+            >
+              Saída
+            </button>
+          </div>
+          <div className={styles.containerBotoesFormulario}>
+            <button
+              className={`${styles.btnFormulario} ${styles.btnCancelar}`}
+              onClick={(e) => {
+                e.preventDefault();
+                setFormularioAberto(false);
+              }}
+            >
+              Cancelar
+            </button>
+            <button
+              className={`${styles.btnFormulario} ${styles.btnAdicionar}`}
+              type="submit"
+            >
+              Adicionar
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
